bfc3446fb80c404bb84c1697250e7b173e574948
Hykilpikonna <admin@moecraft.cc>
2018-09-11 16:33:35 -0400

    [+] 添加Pattern类

---
+++ src/main/java/cc/moecraft/fun/autogitfiti/pattern/Pattern.java
@@ -0,0 +1,21 @@
+package cc.moecraft.fun.autogitfiti.pattern;
+
+import lombok.Data;
+
+import java.io.*;
+import java.text.Normalizer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/15 创建!
+ * Created by Hykilpikonna on 2018/09/15!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class Pattern
+{
+}
