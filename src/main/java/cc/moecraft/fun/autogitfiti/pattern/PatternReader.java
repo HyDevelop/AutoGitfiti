ec6611b4002eaf7fc962df8dfd64eac1d71fa120
Hykilpikonna <admin@moecraft.cc>
2018-09-11 16:33:48 -0400

    [+] 添加PatternReader类

---
+++ src/main/java/cc/moecraft/fun/autogitfiti/pattern/PatternReader.java
@@ -0,0 +1,13 @@
+package cc.moecraft.fun.autogitfiti.pattern;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/15 创建!
+ * Created by Hykilpikonna on 2018/09/15!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+class PatternReader
+{
+}
