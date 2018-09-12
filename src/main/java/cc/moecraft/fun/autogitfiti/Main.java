2d60050e0abcc765e6fe34c837fd28a1882a6a0c
Hykilpikonna <admin@moecraft.cc>
2018-09-12 16:35:53 -0400

    [+] Main类

---
+++ src/main/java/cc/moecraft/fun/autogitfiti/Main.java
@@ -0,0 +1,35 @@
+package cc.moecraft.fun.autogitfiti;
+
+import cc.moecraft.fun.autogitfiti.git.GitUtils;
+import cc.moecraft.fun.autogitfiti.git.Renderer;
+import cc.moecraft.fun.autogitfiti.pattern.Pattern;
+import cc.moecraft.utils.FileUtils;
+import cc.moecraft.utils.cli.Args;
+import cc.moecraft.utils.cli.ArgsUtils;
+import cc.moecraft.utils.cli.ResourceUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+
+import java.io.File;
+import java.util.Map;
+
+import static cc.moecraft.fun.autogitfiti.git.GitUtils.Cache.*;
+import static cc.moecraft.fun.autogitfiti.git.GitUtils.*;
+import static cc.moecraft.fun.autogitfiti.git.Renderer.render;
+import static cc.moecraft.fun.autogitfiti.utils.JarUtils.getJarfileName;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/21 创建!
+ * Created by Hykilpikonna on 2018/09/21!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class Main
+{
+    public static void main(String[] rawArgs) throws Exception
+    {
+
+    }
+}
