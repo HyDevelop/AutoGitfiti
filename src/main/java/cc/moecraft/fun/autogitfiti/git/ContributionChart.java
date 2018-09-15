1b6fc0a616aa54e625ad891538c185db3945612a
Hykilpikonna <admin@moecraft.cc>
2018-09-15 17:07:41 -0400

    [+] 提交表类

---
+++ src/main/java/cc/moecraft/fun/autogitfiti/git/ContributionChart.java
@@ -0,0 +1,18 @@
+package cc.moecraft.fun.autogitfiti.git;
+
+import lombok.Data;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/16 创建!
+ * Created by Hykilpikonna on 2018/09/16!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class ContributionChart
+{
+}
