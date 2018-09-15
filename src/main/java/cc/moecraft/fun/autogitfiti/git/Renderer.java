f55e91af7679ea4c84b7871109fd857779854b6a
Hykilpikonna <admin@moecraft.cc>
2018-09-15 16:50:21 -0400

    [+] 渲染器类

---
+++ src/main/java/cc/moecraft/fun/autogitfiti/git/Renderer.java
@@ -0,0 +1,20 @@
+package cc.moecraft.fun.autogitfiti.git;
+
+import cc.moecraft.fun.autogitfiti.pattern.Pattern;
+import org.eclipse.jgit.api.CommitCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+
+import java.util.Date;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/16 创建!
+ * Created by Hykilpikonna on 2018/09/16!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class Renderer
+{
+}
