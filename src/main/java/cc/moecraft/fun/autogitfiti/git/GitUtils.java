2ed3e8cab792fbd4b8ab993f3c69fe6d7a799be0
Hykilpikonna <admin@moecraft.cc>
2018-09-15 17:03:38 -0400

    [+] Git工具类

---
+++ src/main/java/cc/moecraft/fun/autogitfiti/git/GitUtils.java
@@ -0,0 +1,25 @@
+package cc.moecraft.fun.autogitfiti.git;
+
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import static cc.moecraft.fun.autogitfiti.git.GitUtils.Cache.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/16 创建!
+ * Created by Hykilpikonna on 2018/09/16!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class GitUtils
+{
+}
