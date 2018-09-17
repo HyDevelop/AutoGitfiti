7593af485d7a1fb99aea6bde6f479070fe67ddee
Hykilpikonna <admin@moecraft.cc>
2018-09-17 17:17:39 -0400

    [T] 测试提交表对象

---
+++ src/test/java/ContributionChartTest.java
@@ -0,0 +1,18 @@
+import cc.moecraft.fun.autogitfiti.git.ContributionChart;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/16 创建!
+ * Created by Hykilpikonna on 2018/09/16!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class ContributionChartTest
+{
+    public static void main(String[] args)
+    {
+        ContributionChart chart = new ContributionChart();
+        System.out.println(chart);
+    }
+}
