747648c23482f849c78f7923e4ea831953a42148
Hykilpikonna <admin@moecraft.cc>
2018-09-17 17:08:31 -0400

    [+] 构造时计算每个格子的日期

--- src/main/java/cc/moecraft/fun/autogitfiti/git/ContributionChart.java
+++ src/main/java/cc/moecraft/fun/autogitfiti/git/ContributionChart.java
@@ -17,4 +17,22 @@ import java.util.Date;
 public class ContributionChart
 {
     private Date[][] dateMap = new Date[53][7];
+    private ContributionChart(Date date)
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        // 重置到周日 (表的最下面那一行)
+        while (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY)
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+
+        for (int x = dateMap.length - 1; x >= 0; x--)
+        {
+            for (int y = dateMap[x].length - 1; y >= 0; y--)
+            {
+                dateMap[x][y] = calendar.getTime();
+                calendar.add(Calendar.DAY_OF_YEAR, -1);
+            }
+        }
+    }
 }
