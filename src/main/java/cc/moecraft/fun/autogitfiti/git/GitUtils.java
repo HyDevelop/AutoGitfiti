81a09ff7966168eb4b495abd211a479b54c7c582
Hykilpikonna <admin@moecraft.cc>
2018-09-17 16:56:07 -0400

    [M] Cache移出GitUtils

--- src/main/java/cc/moecraft/fun/autogitfiti/git/GitUtils.java
+++ src/main/java/cc/moecraft/fun/autogitfiti/git/GitUtils.java
@@ -3,14 +3,12 @@ package cc.moecraft.fun.autogitfiti.git;
 import org.eclipse.jgit.api.*;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-import static cc.moecraft.fun.autogitfiti.git.GitUtils.Cache.*;
+import static cc.moecraft.fun.autogitfiti.git.Cache.*;
 
 /**
  * 此类由 Hykilpikonna 在 2018/09/16 创建!
@@ -23,48 +21,6 @@ import static cc.moecraft.fun.autogitfiti.git.GitUtils.Cache.*;
 @SuppressWarnings("WeakerAccess")
 public class GitUtils
 {
-    /**
-     * 缓存类
-     */
-    public static class Cache
-    {
-        public static String authorName = "";
-        public static String authorEmail = "";
-        public static String remote = null;
-        public static String username = null;
-        public static String password = null;
-
-        public static void init(String remote, String username, String password)
-        {
-            Cache.remote = remote;
-            Cache.username = username;
-            Cache.password = password;
-        }
-
-        /**
-         * Author信息缓存
-         */
-        private static final Map<Date, PersonIdent> author = new HashMap<>();
-
-        /**
-         * 因为每个Author信息都包含日期,
-         * 所以按日期缓存好,
-         * 这样如果在一个格子提交50次的话就不用重新构造50个新对象了
-         * @param date 日期
-         * @return Author
-         */
-        public static PersonIdent getAuthor(Date date)
-        {
-            if (author.containsKey(date)) return author.get(date);
-            author.put(date, new PersonIdent(authorName, authorEmail, date, TimeZone.getTimeZone("UTC")));
-            return getAuthor(date);
-        }
-
-        /**
-         * 提前计算好格子的日期
-         */
-        private static final ContributionChart contributionChart = new ContributionChart();
-    }
 
     /**
      * 获取格子的日期
